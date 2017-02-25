14318958fa093e9339f1731580d2b7e4c083bbb7
psykidellic <riteshn@gmail.com>
2017-02-24 21:42:08 -0800

    get sentiment with api working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """`main` is the top level module for your Flask application."""
 # Import the Flask Framework
 from flask import Flask
-from flask import url_for, redirect
+from flask import request, jsonify
 
 from googleapiclient.discovery import build
 
@@ -14,6 +14,31 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     return app.send_static_file('index.html')
+
+def _analyze(text, type='analyzeSentiment'):
+    body = {
+        'document': {
+            'type': 'PLAIN_TEXT',
+            'content': text,
+        },
+        'encoding_type': 'UTF32'
+    }
+
+    service = build('language', 'v1')
+
+    meth = getattr(service.documents(), type)
+    request = meth(body=body)
+    response = request.execute()
+
+    return response
+
+
+@app.route('/analyze', methods=['POST'])
+def analyzer():
+    q = request.get_json()
+    t = q['text']
+
+    return jsonify(_analyze(t))
 
 
 @app.errorhandler(404)
