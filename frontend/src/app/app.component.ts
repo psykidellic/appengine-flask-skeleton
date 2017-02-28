683d15aa5f8c26c948324c352e49cec8a44e1c82
psykidellic <riteshn@gmail.com>
2017-02-27 23:15:31 -0800

    ghetto http post to get data
    apparantly angular2 requires the whole service/component shebang.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Headers, Response} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public text = ""
+  private analyzeUrl = "/analyze"
+
+  constructor (private http: Http) {}
+
+  analyzeText (event) {
+    let data: any;
+    data = this.http.post(this.analyzeUrl, {'text': this.text})
+             .toPromise()
+             .then(this.extractData)
+             .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Promise.reject(errMsg);
+  }
 }
