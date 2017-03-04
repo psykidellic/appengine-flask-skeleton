a7c59271191647965c236668a04acb0e68c55110
psykidellic <riteshn@gmail.com>
2017-03-03 20:21:13 -0800

    add sentiment to the mix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
   public text = "";
   public language = "";
   public magnitude = "";
-  public score = "";
+  public score: number = 0.0;
+  public sentiment = "";
   errorMessage: string;
   private analyzeUrl = "/analyze"
 
@@ -28,7 +29,19 @@ export class AppComponent {
   private extractData(res: any) {
     this.language = res.language
     this.magnitude = res.documentSentiment.magnitude
-    this.score = res.documentSentiment.score
+    var score = this.score = res.documentSentiment.score
+
+    if (score < -0.5) {
+      this.sentiment = 'Clearly negative'
+    } else if (score > -0.5 && score <= 0.1) {
+      this.sentiment = 'Neutral'
+    } else if (score > 0.1 && score <= 0.3) {
+      this.sentiment = 'Mixed'
+    } else if (score > 0.3 && score <= 0.8) {
+      this.sentiment = 'Positive'
+    } else {
+      this.sentiment = 'Clearly positive'
+    }
   }
 
 
